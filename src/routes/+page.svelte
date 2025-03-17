1f1cf16cac42a440796e10dec56d218f8af8cd0e
Corey <coreyg142@gmail.com>
2025-03-16 20:31:26 -0400

    experimenting with fonts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,36 @@
-<script>
+<script lang="ts">
   import Fa from "svelte-fa";
   import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 </script>
 
 <div class="flex h-screen">
   <div class="m-auto">
-    <h1>Under Construction</h1>
+    <h1>
+      Corey <span class="font-bold">W Gross</span>
+    </h1>
+    <h2>Software Engineer</h2>
     <p>
       In the meantime, please visit <a href="https://www.github.com/coreyg142">
-        <Fa icon={faGithub} class="inline-block text-2xl" />
+        <Fa icon={faGithub} class="inline-icon" />
       </a>
       or
       <a href="https://www.linkedin.com/in/coreyg142/">
-        <Fa icon={faLinkedin} class="inline-block text-2xl " />
+        <Fa icon={faLinkedin} class="inline-icon" />
       </a>
     </p>
   </div>
 </div>
 
-<style lang="scss">
+<style lang="postcss">
+  @reference "tailwindcss";
   h1 {
-    font-size: var(--text-4xl);
-    font-family: "Ubuntu", sans-serif;
-    font-weight: 700;
-    font-style: normal;
+    @apply text-[3rem] text-purple-600 font-[Workbench];
   }
-
+  h2 {
+    @apply text-[2rem] text-purple-500 font-[Nunito];
+    /* font-family: "Flow Circular", system-ui; */
+  }
   p {
-    font-size: var(--text-lg);
-    font-family: "Nunito", sans-serif;
-    font-weight: 400;
-    font-style: normal;
+    @apply text-[1.2rem] text-gray-700 font-[Nunito];
   }
 </style>
