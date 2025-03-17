e34e266950fefe4f6f0c42bc57b335f8585eaf76
Corey <coreyg142@gmail.com>
2025-03-16 21:15:17 -0400

    animated name on load

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,46 @@
 <script lang="ts">
   import Fa from "svelte-fa";
   import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+  import { onMount } from "svelte";
+
+  const name = "Corey W Gross";
+  const nameSplit = name.split("");
+  const transitioned = new Array(name.length).fill(false);
+
+  onMount(() => {
+    setTimeout(() => {
+      nameSplit.forEach((_, i) => {
+        setTimeout(() => {
+          transitioned[i] = true;
+        }, i * 150);
+      });
+    }, 500);
+  });
 </script>
 
 <div class="flex h-screen">
   <div class="m-auto">
     <h1>
-      Corey <span class="font-bold">W Gross</span>
+      {#each nameSplit as char, i}
+        <span
+          class={transitioned[i] ? "font-[Workbench]" : "font-[Flow_Circular]"}
+        >
+          {char}
+        </span>
+      {/each}
     </h1>
-    <h2>Software Engineer</h2>
+    <!-- <h1>
+      Corey <span class="font-bold">W Gross</span>
+    </h1> -->
     <p>
-      In the meantime, please visit <a href="https://www.github.com/coreyg142">
+      In the meantime, please visit <a
+        href="https://www.github.com/coreyg142"
+        aria-label=" My GitHub"
+      >
         <Fa icon={faGithub} class="inline-icon" />
       </a>
       or
-      <a href="https://www.linkedin.com/in/coreyg142/">
+      <a href="https://www.linkedin.com/in/coreyg142/" aria-label="My LinkedIn">
         <Fa icon={faLinkedin} class="inline-icon" />
       </a>
     </p>
@@ -24,7 +50,7 @@
 <style lang="postcss">
   @reference "tailwindcss";
   h1 {
-    @apply text-[3rem] text-purple-600 font-[Workbench];
+    @apply text-[3rem] text-purple-600 font-[Flow_Circular];
   }
   h2 {
     @apply text-[2rem] text-purple-500 font-[Nunito];
