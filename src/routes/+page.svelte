20cbe7a92c76b41031127e34daa0341623355c03
Corey <coreyg142@gmail.com>
2025-03-16 21:18:59 -0400

    Squashed commit of the following:
    commit b00400e60e6f854e727079e4c1337580d86ffc9a
    Author: Corey <coreyg142@gmail.com>
    Date:   Sun Mar 16 21:17:20 2025 -0400
    
        adjusted timing
    
    commit e34e266950fefe4f6f0c42bc57b335f8585eaf76
    Author: Corey <coreyg142@gmail.com>
    Date:   Sun Mar 16 21:15:17 2025 -0400
    
        animated name on load
    
    commit 1f1cf16cac42a440796e10dec56d218f8af8cd0e
    Author: Corey <coreyg142@gmail.com>
    Date:   Sun Mar 16 20:31:26 2025 -0400
    
        experimenting with fonts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,62 @@
-<script>
+<script lang="ts">
   import Fa from "svelte-fa";
   import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+  import { onMount } from "svelte";
+
+  const name = "Corey W Gross";
+  const nameSplit = name.split("");
+  const transitioned = new Array(name.length).fill(false);
+
+  onMount(() => {
+    setTimeout(() => {
+      nameSplit.forEach((_, i) => {
+        setTimeout(() => {
+          transitioned[i] = true;
+        }, i * 150);
+      });
+    }, 250);
+  });
 </script>
 
 <div class="flex h-screen">
   <div class="m-auto">
-    <h1>Under Construction</h1>
+    <h1>
+      {#each nameSplit as char, i}
+        <span
+          class={transitioned[i] ? "font-[Workbench]" : "font-[Flow_Circular]"}
+        >
+          {char}
+        </span>
+      {/each}
+    </h1>
+    <!-- <h1>
+      Corey <span class="font-bold">W Gross</span>
+    </h1> -->
     <p>
-      In the meantime, please visit <a href="https://www.github.com/coreyg142">
-        <Fa icon={faGithub} class="inline-block text-2xl" />
+      In the meantime, please visit <a
+        href="https://www.github.com/coreyg142"
+        aria-label=" My GitHub"
+      >
+        <Fa icon={faGithub} class="inline-icon" />
       </a>
       or
-      <a href="https://www.linkedin.com/in/coreyg142/">
-        <Fa icon={faLinkedin} class="inline-block text-2xl " />
+      <a href="https://www.linkedin.com/in/coreyg142/" aria-label="My LinkedIn">
+        <Fa icon={faLinkedin} class="inline-icon" />
       </a>
     </p>
   </div>
 </div>
 
-<style lang="scss">
+<style lang="postcss">
+  @reference "tailwindcss";
   h1 {
-    font-size: var(--text-4xl);
-    font-family: "Ubuntu", sans-serif;
-    font-weight: 700;
-    font-style: normal;
+    @apply text-[3rem] text-purple-600 font-[Flow_Circular];
   }
-
+  h2 {
+    @apply text-[2rem] text-purple-500 font-[Nunito];
+    /* font-family: "Flow Circular", system-ui; */
+  }
   p {
-    font-size: var(--text-lg);
-    font-family: "Nunito", sans-serif;
-    font-weight: 400;
-    font-style: normal;
+    @apply text-[1.2rem] text-gray-700 font-[Nunito];
   }
 </style>
